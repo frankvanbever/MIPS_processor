c19ac4195cfa770afc4485bb1d10c7603f2a1588
Frank Vanbever <frank.vanbever@gmail.com>
2013-03-06 19:32:12 +0100

    finished testbench for MUX

--- testbenches/mux_tb.vhd
+++ testbenches/mux_tb.vhd
@@ -32,25 +32,27 @@ architecture behavior of mux_tb is
 
 	signal clk : std_logic := '0';
 
-	signal tb_vec_in_1 : std_logic;
-	signal tb_vec_in_2 : std_logic;
+	signal tb_vec_in_1 : std_logic_vector(31 downto 0);
+
+	signal tb_vec_in_2 : std_logic_vector(31 downto 0);
+
 
 	signal tb_selector : std_logic;
 
-	signal tb_vec_out : std_logic;
+	signal tb_vec_out : std_logic_vector(31 downto 0);
 
 	constant clk_period : time := 10 ns;
 
 begin
 	uut : MUX  port map(
-		clk => clk,
-		selector => tb_selector,
-		vector_in_1 => tb_vec_in_1,
-		vector_in_2 => tb_vec_in_2,
-		vector_out  => tb_vec_out
-	);
+				   clk => clk,
+				   selector => tb_selector,
+				   vector_in_1 => tb_vec_in_1,
+				   vector_in_2 => tb_vec_in_2,
+				   vector_out  => tb_vec_out
+			   );
 
-	
+
 	clk_process :process
 	begin
 		clk <= '0';
@@ -62,22 +64,22 @@ begin
 	stim_proc : process
 	begin
 		wait for 100 ns;
-		
+
 		wait for clk_period*10;
 
-		tb_selector <= '0'
+		tb_selector <= '0';
 		tb_vec_in_1 <= X"FFFFFFFF";
 		tb_vec_in_2 <= X"00000000";
-		
-		wait until rising_edge(clk);
+
+		wait for clk_period; 
 
 		assert tb_vec_out = X"FFFFFFFF" report "first test error";
 
-		wait until rising_edge(clk);
+		wait until rising_edge(clk); 
 
 		tb_selector <= '1';
 
-		wait until rising_edge(clk);
+		wait for clk_period*2; 
 
 		assert tb_vec_out = X"00000000" report "second test error";
 
