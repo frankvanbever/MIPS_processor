5b4658eb6246095298aa6e7c3f3821a0f8913f70
Steven Vanden Branden <stevenvandenbranden@gmail.com>
2013-03-13 16:42:29 +0100

    Added control module and testbench (branch not equal bug)

---
+++ Control.vhd
@@ -0,0 +1,113 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 		Steven Vanden Branden
+-- 
+-- Create Date:    15:23:25 03/13/2013 
+-- Design Name: 
+-- Module Name:    Control - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Control is
+    Port ( Instruction : in  STD_LOGIC_VECTOR (31 downto 26);
+			  Instruction_funct : in  STD_LOGIC_VECTOR (5 downto 0);
+           RegDst : out  STD_LOGIC;
+           ALUSrc : out  STD_LOGIC;
+           MemtoReg : out  STD_LOGIC;
+           RegWrite : out  STD_LOGIC;
+           MemRead : out  STD_LOGIC;
+           MemWrite : out  STD_LOGIC;
+           Branch : out  STD_LOGIC;
+			  Branch_ne : out  STD_LOGIC;
+           ALUop : out  STD_LOGIC_VECTOR (1 downto 0));
+end Control;
+
+architecture Behavioral of Control is
+
+begin
+
+	Control_out: process(Instruction)
+		begin
+		case Instruction is
+			when "000000" => --R-format instruction
+			RegDst<= '1';
+			ALUSrc<=	'0';
+			MemtoReg<= '0';
+			RegWrite<= '1';
+			MemRead<='0';
+			MemWrite<='0';
+			Branch<='0';
+			Branch_ne<='0';
+			ALUop<="10";
+			when "100011" => --load word instruction
+			RegDst	<= '0';
+			ALUSrc	<=	'1';
+			MemtoReg	<= '1';
+			RegWrite	<= '1';
+			MemRead	<= '1';
+			MemWrite	<= '0';
+			Branch	<= '0';
+			Branch_ne<='0';
+			ALUop		<= "00";
+			when "101011" => -- save word instruction
+			RegDst	<= '0';
+			ALUSrc	<=	'1';
+			MemtoReg	<= '0';
+			RegWrite	<= '0';
+			MemRead	<= '0';
+			MemWrite	<= '1';
+			Branch	<= '0';
+			Branch_ne<='0';
+			ALUop		<= "00";
+			when "000100" =>	-- branch on equal
+			RegDst	<= '0';
+			ALUSrc	<=	'0';
+			MemtoReg	<= '0';
+			RegWrite	<= '0';
+			MemRead	<= '0';
+			MemWrite	<= '0';
+			if(Instruction_funct="000100")then
+			Branch	<= '1';
+			Branch_ne<= '0';
+			else
+			Branch	<= '0';
+			Branch_ne<= '1';
+			end if;
+			ALUop		<= "01";
+			when others => --error
+			RegDst	<= '0';
+			ALUSrc	<=	'0';
+			MemtoReg	<= '0';
+			RegWrite	<= '0';
+			MemRead	<= '0';
+			MemWrite	<= '0';
+			Branch	<= '0';
+			Branch_ne<='0';
+			ALUop		<= "00"; 
+		end case;
+		
+		end process Control_out;
+
+end Behavioral;
+
---
+++ testbenches/Control_tb.vhd
@@ -0,0 +1,133 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   16:24:09 03/13/2013
+-- Design Name:   
+-- Module Name:   /home/steven/Documenten/Codes/pcarch/MIPSmodules/Control_tb.vhd
+-- Project Name:  MIPSmodules
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Control
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Control_tb IS
+END Control_tb;
+ 
+ARCHITECTURE behavior OF Control_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Control
+    PORT(
+         Instruction : IN  std_logic_vector(31 downto 26);
+         Instruction_funct : IN  std_logic_vector(5 downto 0);
+         RegDst : OUT  std_logic;
+         ALUSrc : OUT  std_logic;
+         MemtoReg : OUT  std_logic;
+         RegWrite : OUT  std_logic;
+         MemRead : OUT  std_logic;
+         MemWrite : OUT  std_logic;
+         Branch : OUT  std_logic;
+         Branch_ne : OUT  std_logic;
+         ALUop : OUT  std_logic_vector(1 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Instruction : std_logic_vector(31 downto 26) := (others => '0');
+   signal Instruction_funct : std_logic_vector(5 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal RegDst : std_logic;
+   signal ALUSrc : std_logic;
+   signal MemtoReg : std_logic;
+   signal RegWrite : std_logic;
+   signal MemRead : std_logic;
+   signal MemWrite : std_logic;
+   signal Branch : std_logic;
+   signal Branch_ne : std_logic;
+   signal ALUop : std_logic_vector(1 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   constant clk_period : time := 10 ns;
+	signal clk: std_logic;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Control PORT MAP (
+          Instruction => Instruction,
+          Instruction_funct => Instruction_funct,
+          RegDst => RegDst,
+          ALUSrc => ALUSrc,
+          MemtoReg => MemtoReg,
+          RegWrite => RegWrite,
+          MemRead => MemRead,
+          MemWrite => MemWrite,
+          Branch => Branch,
+          Branch_ne => Branch_ne,
+          ALUop => ALUop
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for clk_period*10;
+
+      -- insert stimulus here
+      wait for clk_period*10;
+		Instruction<="000000";			--testfor R-format
+		wait for clk_period*10;
+		Instruction<="100011";			--testfor load word
+		wait for clk_period*10;
+		Instruction<="101011";			--testfor store word
+		wait for clk_period*10;
+		Instruction_funct<="000100";
+		Instruction<="000100";			--testfor branch equal
+		wait for clk_period*10;
+		Instruction_funct<="000101"; 	--testfor branch not equal
+		wait for clk_period*10;
+		Instruction<="111111";			--testfor error
+		
+
+      wait;
+   end process;
+
+END;
