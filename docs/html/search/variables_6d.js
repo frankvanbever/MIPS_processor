bc043b377922a28dfaba44d501102da5330f69f7
Frank Vanbever <frank.vanbever@gmail.com>
2013-04-17 14:05:09 +0200

    updated docs

--- docs/html/search/variables_6d.js
+++ docs/html/search/variables_6d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['21memwrite',['21MemWrite',['../classdata__memory.html#a20b873e427ea4a28cb74ec7d495730d8',1,'data_memory']]],
-  ['22memread',['22MemRead',['../classdata__memory.html#a24c8cc235a5d3a2ce36383b15a577f8e',1,'data_memory']]]
+  ['13memtoreg',['13MemtoReg',['../class_control.html#ae18caf473070cf8e735d12ca860fb762',1,'Control']]],
+  ['15memread',['15MemRead',['../class_control.html#a703e49c8be2a086810119a4f32c2edd0',1,'Control.15MemRead()'],['../classdata__memory.html#a24c8cc235a5d3a2ce36383b15a577f8e',1,'data_memory.22MemRead()']]],
+  ['16memwrite',['16MemWrite',['../class_control.html#af268f4bd077313d14130fa77402beccb',1,'Control.16MemWrite()'],['../classdata__memory.html#a20b873e427ea4a28cb74ec7d495730d8',1,'data_memory.21MemWrite()']]]
 ];
